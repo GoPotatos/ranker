874621db15ab6e9c8b582f6a40d395c3c2bce647
GoPotatos <awabelmahe@gmail.com>
2021-03-23 01:05:07 +0200

    Reducing timeout to 10 seconds

--- content.js
+++ content.js
@@ -5,7 +5,7 @@ window.onload=()=>{
 	links=Array.from(document.querySelectorAll("[data-ads] .text-sm")).map(item=>item.innerText)
 	notFound=document.querySelector(".leading-none")
 	counter++;
-	if(links.length||!notFound||counter>=20){
+	if(links.length||!notFound||counter>=10){
 		clearInterval(timer)
 		chrome.runtime.sendMessage(chrome.runtime.id,{type:"links",links},res=>{})
 		}
